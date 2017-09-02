1fd8aa1f50f0c05c75857c99b99e0c23e9d81dd9
MiniZhuwei <minizhuwei@gmail.com>
2017-09-02 21:59:28 +0800

    Data purify implementation and hard coded test

--- Shifu-Spark-Eval/main/scala/ml/shifu/shifu/spark/eval/DataPurifier.scala
+++ Shifu-Spark-Eval/main/scala/ml/shifu/shifu/spark/eval/DataPurifier.scala
@@ -1,64 +1,75 @@
 package ml.shifu.shifu.spark.eval
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
 import org.apache.commons.jexl2.{Expression, JexlEngine, JexlException, MapContext}
+import com.google.common.base.Splitter
+import scala.collection.JavaConverters._
 import org.apache.commons.lang.StringUtils
 
 trait DataPurifier {
 
-        val inputRDD : RDD[String]
+    var inputRDD : RDD[String] = _
 
-        val headers : Array[String]
+    var headers : Array[String] = _
 
-        val delimiter : String
+    var delimiter : String = _
 
-        val filterExp : String
+    var filterExp : Option[String] = _
 
-        val context : SparkContext
-        def init()
+    def init()
 
-        def purify() : RDD[String] = {
-            purify(intputRDD, headers, delimiter, filterExp)
-        }
+    def purify() : RDD[String] = {
+        init
+        purify(inputRDD, headers, delimiter, filterExp) 
+    }
 
         
 
-        private[this] def purify(inputRDD : RDD[String], headers : Array[String], delimiter : String, filterExp : Option[String]): RDD[String] = {
-            filterExp match {
-                case Some(exp) => inputRDD.mapPartitionsWithIndex { (index, iterator) => {
-                        val jexl = new JexlEngine()
-                        val jc = new MapContext()
-                        val jexlExpression = try {
-                            Some(jexl.createExpression(exp))
+    private[this] def purify(inputRDD : RDD[String], headers : Array[String], delimiter : String, filterExp : Option[String]): RDD[String] = {
+        filterExp match {
+            case Some(exp) => inputRDD.mapPartitionsWithIndex { (index, iterator) => {
+                val jexl = new JexlEngine()
+                val jc = new MapContext()
+                val jexlExpression = {
+                    if(StringUtils.isBlank(exp)) {
+                        None
+                    } else {
+                        try {
+                        Option(jexl.createExpression(exp))
                         } catch {
                             case _ => None
                         }
+                    }
+                }
 
-                        if(index == 0) {
-                            iterator.drop(1)
-                        }
-                        val filterFunc : (Array[String] => Boolean) = { inputData : Array[String] => {
-                                jexlExpression match {
-                                    case None => true
-                                    case Some(exp) => {
-                                        for(i <- 0 until headers.length) {
-                                            jc.set(headers(i), inputData(i))
-                                        }
-                                        try {
-                                            Boolean.unbox(exp.evaluate(jc))
-                                        } catch {
-                                            case _  => false
-                                        }
+                    if(index == 0) {
+                        iterator.drop(1)
+                    }
+                    val filterFunc : (Array[String] => Boolean) = { inputData : Array[String] => {
+                        jexlExpression match {
+                            case None => true
+                            case Some(jexp) => {
+                                if(headers.length != inputData.length) {
+                                    false
+                                } else {
+                                    for(i <- 0 until headers.length) {
+                                        jc.set(headers(i), inputData(i))
+                                    }
+                                    try {
+                                        Boolean.unbox(jexp.evaluate(jc))
+                                    } catch {
+                                        case _  => false
                                     }
                                 }
                             }
                         }
-                        iterator.filter(line => filterFunc(StringUtils.split(line, delimiter)))
-                    }
-                }
-                case None => inputRDD
-            }
+                    }}
+                    iterator.filter(line => filterFunc(Splitter.on(delimiter).split(line).asScala.toArray))
+            }}
+            case _ => inputRDD
         }
+    }
 }
 
 
