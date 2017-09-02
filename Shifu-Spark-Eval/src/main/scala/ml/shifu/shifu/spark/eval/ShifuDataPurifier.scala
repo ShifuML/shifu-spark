5567f2d5e9111bfff6d64b9ebc4672bff7a381b3
MiniZhuwei <minizhuwei@gmail.com>
2017-09-02 21:59:28 +0800

    Data purify implementation and hard coded test

--- Shifu-Spark-Eval/src/main/scala/ml/shifu/shifu/spark/eval/ShifuDataPurifier.scala
+++ Shifu-Spark-Eval/src/main/scala/ml/shifu/shifu/spark/eval/ShifuDataPurifier.scala
@@ -2,20 +2,37 @@ package ml.shifu.shifu.spark.eval
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import ml.shifu.shifu.container.obj.EvalConfig
-import ml.shifu.shifu.container.obj.ModelConfig
+import ml.shifu.shifu.container.obj.{ModelConfig, EvalConfig}
+import ml.shifu.shifu.container.obj.RawSourceData.SourceType
 import ml.shifu.shifu.util.CommonUtils
 
 
 
-public class ShifuDataPurifier(modelConfig : ModelConfig, inputPath : String) extends DataPurifier {
+class ShifuDataPurifier(modelConfig : ModelConfig, inputPath : String, context : SparkContext) extends DataPurifier {
 
+    /*
+    var inputRDD : RDD[String]
+
+    var headers : Array[String]
+
+    var delimiter : String
+
+    var filterExp : Option[String]
+    */
     override def init() {
         this.headers = CommonUtils.getFinalHeaders(modelConfig)
-        this.delimieter = modelConfig.getDataSetDelimiter
-        this.filterExp = modelConfig.getFilterExpression
-        this.context = new SparkContext
+        this.delimiter = modelConfig.getDataSetDelimiter
+        this.filterExp = Option(modelConfig.getFilterExpressions)
         this.inputRDD = context.textFile(inputPath)
     }
 
+}
+
+
+object ShifuDataPurifier {
+
+    def apply(modelConfigPath : String, sourceType : SourceType, inputPath : String, context : SparkContext) : ShifuDataPurifier = {
+        val modelConfig = CommonUtils.loadModelConfig(modelConfigPath, sourceType)
+        new ShifuDataPurifier(modelConfig, inputPath, context)
+    }
 }
