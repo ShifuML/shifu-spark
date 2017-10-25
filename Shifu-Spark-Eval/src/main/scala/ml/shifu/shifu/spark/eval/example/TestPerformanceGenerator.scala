d32d37ec2ab46d264fdcb52d711f9da1dcf166bd
MiniZhuwei <minizhuwei@gmail.com>
2017-10-25 14:39:00 +0800

    add example spark job for data purification, model evaluation and performnace generating

---
+++ Shifu-Spark-Eval/src/main/scala/ml/shifu/shifu/spark/eval/example/TestPerformanceGenerator.scala
@@ -0,0 +1,28 @@
+package ml.shifu.shifu.spark.eval.example
+
+import ml.shifu.shifu.spark.eval.{Eval, ShifuEval, PerformanceGenerator}
+import ml.shifu.shifu.container.obj.RawSourceData.SourceType
+import ml.shifu.shifu.util.{CommonUtils, Constants}
+
+import org.apache.spark.{SparkContext, Accumulator}
+
+import org.apache.hadoop.fs.Path
+
+import scala.collection.mutable.HashMap
+
+object TestPerformanceGenerator {
+
+    def main(arg : Array[String]) {
+
+        val context = new SparkContext
+        val accumMap = new HashMap[String, Accumulator[Long]]
+        val shifuEval = new ShifuEval(SourceType.LOCAL, "./ModelConfig.json", "./ColumnConfig.json", "Eval2", context, accumMap)
+        val broadcastModelConfig = context.broadcast(shifuEval.modelConfig)
+        val broadcastEvalConfig = context.broadcast(shifuEval.evalConfig)
+        val modelNum = CommonUtils.getBasicModelsCnt(shifuEval.modelConfig, shifuEval.evalConfig, shifuEval.evalConfig.getDataSet.getSource)
+        val scoreRDD = shifuEval.eval
+        val perfGen = new PerformanceGenerator(broadcastModelConfig, broadcastEvalConfig, context, accumMap, modelNum)
+        perfGen.genPerfByModels(scoreRDD)
+        context.stop
+    }
+}
