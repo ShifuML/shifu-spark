3061ab0ec3d0d5d9f36b77a5ae928a9787089565
wzhu1 <wzhu1@paypal.com>
2017-12-27 14:04:55 +0800

    add copy dependency plugin in pom and package shifu spark script

--- shifu-spark-eval/src/main/bash/package-shifu.sh
+++ shifu-spark-eval/src/main/bash/package-shifu.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
-if [ ! -d "./shifu" ]; then
-    git clone -b develop git@github.com:ShifuML/shifu.git
-    cd shifu
-    mvn -DskipTests clean install
+
+SHIFU_PACKAGE_PATH=`find . -iname shifu*-hdp-yarn.tar.gz`
+
+echo "${SHIFU_PACKAGE_PATH}"
+
+SHIFU_PACKAGE_NAME=`basename "${SHIFU_PACKAGE_PATH}"`
+
+
+if [ -f "${SHIFU_PACKAGE_PATH}" ]; then
+   TARGET_PATH=`dirname "${SHIFU_PACKAGE_PATH}"`
+   cd "${TARGET_PATH}"
+   SHIFU_SPARK_JAR=`find . -iname shifu-spark*.jar | grep -v "source" | grep -v docs`
+   if [ -f ${SHIF_SPARK_JAR} ]; then
+        rm -rf tmp
+        mkdir tmp
+        tar -zxvf "${SHIFU_PACKAGE_NAME}" -C tmp
+        SHIFU_TMP_DIR=`ls tmp`
+        cp "${SHIFU_SPARK_JAR}" "tmp/${SHIFU_TMP_DIR}/lib/"
+        tar -zcvf "${SHIFU_PACKAGE_NAME}" -C tmp .
+   else 
+        echo "Could not find shifu spark eval jar. Please build shifu spark before run this script."
+   fi
+else 
+    echo "Could not find shifu package in target. Please build shifu spark before run this script"
 fi
